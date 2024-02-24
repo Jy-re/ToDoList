763f462d2e41c1c5340d951364a2ee84a10afd5d
Jerilyn Yare <102701655+Jy-re@users.noreply.github.com>
2024-02-24 20:00:46 +0800

    added function comments

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -4,13 +4,13 @@ import { Item } from './Item';
 import { EditItem } from './EditItem';
 
 export const List = () => {
-    // Load todos from localStorage on component mount
+    // Gets the todos that was saved in the local storage
     const [todos, setTodos] = useState(() => {
         const storedTodos = localStorage.getItem('todos');
         return storedTodos ? JSON.parse(storedTodos) : [];
     });
 
-    // Save todos to localStorage whenever todos change
+    // Save todos to localStorage when todos change
     useEffect(() => {
         localStorage.setItem('todos', JSON.stringify(todos));
     }, [todos]);
@@ -30,14 +30,17 @@ export const List = () => {
         setTodos(todos.map((todo, i) => i === index ? {...todo, completed: !todo.completed} : todo));
     };
 
+    // If i matches an index from our array, it sets the isEditing to true
     const editItem = index => {
         setTodos(todos.map((todo, i) => i === index ? {...todo, isEditing: !todo.isEditing} : todo));
     };
 
+    // if our chosen i matches an index from our array it assigns a new task and exits the editing mode by setting the already true isEditing to false 
     const editTask = (newTask, index) => {
         setTodos(todos.map((todo, i) => i === index ? { ...todo, task: newTask, isEditing: false } : todo));
     };
 
+    // the filter function creates a new array based on the condition so if our chosen i doesn't match the index of an item in our array, it excludes that item from the new array which then, deletes it
     const deleteItem = index => {
         setTodos(todos.filter((todo, i) => i !== index));
     };
